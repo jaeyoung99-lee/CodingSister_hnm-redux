5fc29651f4eab663df55e165fc1b22f604801a32
jaeyoung99-lee <leejy0715@naver.com>
2024-08-31 18:09:57 +0900

    Devtool

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,11 @@ import { applyMiddleware, createStore } from "redux";
 import { thunk } from "redux-thunk";
 import rootReducer from "./reducers/index";
 // import rootReducer from "./reducers"; // index.js 파일이 기본 파일이라 폴더명만 써도 index.js 파일을 읽어옴
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-let store = createStore(rootReducer, applyMiddleware(thunk));
+let store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
